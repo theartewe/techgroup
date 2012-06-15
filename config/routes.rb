e75ffa791fd653354a8e253327d15559b6ba62c3
Anthony Blackwell <anthony@slatestudio.com>
2012-06-15 16:03:54 -0700

    Revised routes for corporation

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,23 @@ Techgroup::Application.routes.draw do
   ActiveAdmin.routes(self)
   
   match "change-locale" => 'flatpages#change_locale', :as => :change_locale
-
-  match 'corporation'   => 'corporation#index'   
+  
+  resources :corporation, :except => [:new,:edit,:delete] do
+    collection do
+      match 'board-of-directors'    => 'corporation#board_of_directors',  :as => :board_of_directors
+      match 'ceo-message'           => 'corporation#ceo_message',         :as => :ceo_message
+      match 'clients'               => 'corporation#clients',             :as => :clients
+      match 'corporate-partners'    => 'corporation#corporate_partners',  :as => :corporate_partners
+    end
+  end
+  
   resources :companies, :except => [:new,:edit,:delete] do
     
   end
   resources :porfolio, :except => [:new,:edit,:delete]
   resources :media, :except => [:new,:edit,:delete] do
      get 'search'
-     match 'media/:category'   => 'media#category'
+     match ':category'   => 'media#category'
   end
   match 'contact'   => 'contact#index'
 

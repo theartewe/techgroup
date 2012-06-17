8f8876143d01a8ca0b5d33415b0799d2f690112f
Anthony Blackwell <anthony@slatestudio.com>
2012-06-17 14:18:48 -0700

    Stubbed corporation sub pages

--- app/controllers/corporation_controller.rb
+++ app/controllers/corporation_controller.rb
@@ -1,5 +1,22 @@
 class CorporationController < WebsiteController
   def index
-     @objects = CorporationHistoryEntry.all()
+    @objects = CorporationHistoryEntry.all()
   end
+  
+  def board_of_directors
+   
+  end
+  
+  def ceo_message
+     
+  end
+  
+  def clients
+     
+  end
+  
+  def corporate_partners
+     
+  end
+  
 end

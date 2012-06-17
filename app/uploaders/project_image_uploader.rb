dcedc49887428dcb25c7e9a69e9d23de15626bab
Anthony Blackwell <anthony@slatestudio.com>
2012-06-17 13:55:13 -0700

    Added Project model

---
+++ app/uploaders/project_image_uploader.rb
@@ -0,0 +1,10 @@
+class ProjectImageUploader < CarrierWave::Uploader::Base
+  def store_dir
+    "project-images/#{model.id}"
+  end
+  
+  version :thumbnail do
+    process :resize_to_fill => [232, 189]
+    process :quality => 80
+  end
+end

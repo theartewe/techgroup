7ddff0ecf6e0ce1f04e35ddf755bf1679b712843
Anthony Blackwell <anthony@slatestudio.com>
2012-06-17 14:18:34 -0700

    Stubbed career page

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -1,13 +1,5 @@
-class HomeController < WebsiteController
+class CareerController < WebsiteController
   def index
-
-    language  = "english"
-    @slides = Slide.all.collect do |s|
-        { :text1 => s.get_property(language, "text1").html_safe,
-          :text2 => s.get_property(language, "text2").html_safe,
-          :text3 => s.get_property(language, "text3").html_safe,
-          :text4 => s.get_property(language, "text4").html_safe,
-          :text5 => s.get_property(language, "text5").html_safe }
-    end
+    
   end
 end

8556f20e20d2424d52d2a3f7152ae911e9df77e4
anthonyb <anthony@slatestudio.com>
2012-06-20 06:51:48 -0700

    Fixed company sections

--- app/controllers/company_sections_controller.rb
+++ app/controllers/company_sections_controller.rb
@@ -2,6 +2,5 @@ class CompanySectionsController < WebsiteController
   def show
     @object = CompanySection.find_by_slug!(params[:id])
     @subsections = @object.company_subsections
-    render 'companies/show_section'
   end
 end

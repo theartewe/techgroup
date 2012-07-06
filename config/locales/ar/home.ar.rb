14f96b06bff7a47216596ba2ccf82738ace1046f
anthonyb <anthony@slatestudio.com>
2012-07-06 09:47:08 -0700

    Updated arabic text

---
+++ config/locales/ar/home.ar.rb
@@ -0,0 +1,14 @@
+{
+  :ar => 
+  {
+    :mission_and_vision_header  => "Mission + Vision",
+    :mission_and_vision_cta     => "Tech Group is committed to became the UAE's leading partner in infrastructure development and contributed to the economy through focused diversification.",
+    :our_companies_header       => "Our Companies",  
+    :our_companies_cta          => "Tech Group is comprised of various operating companies that are well-known and highly respected in the field of construction, real estate, vehicle repair, ceramic tiles and wood-joinery.",
+    :partners_header            => "Our Partners",
+    :partners_cta               => "Behind the AED 15 billion Emirates City, is its master developer R Holding. It is actively engaged in real estate & development, hotel & hotel management, construction, and more.",
+    :offices_header             => "مكاتبنا",
+    :offices_cta                => "وتوسع مجموعة تك هو امر لا مفر منه بسبب التزامها بالخدمة في مجلس التعاون الخليجي. ومكاتبها الحالية تقع في موقع محوري لابرام الصفقات مع العملاء بصورة فعالة.
+",
+  }
+}
---
+++ config/locales/ar/media.ar.rb
@@ -0,0 +1,12 @@
+{
+  :ar =>
+  {
+    :media            => "Media",
+    :all              => "all",
+    :search_media     => "Search Media",
+    :view_more_posts  => "view more posts",
+    :posted_under     => "posted under",
+    :searched_for     => "Searched For",
+    :careers          => "Careers"
+  }
+}

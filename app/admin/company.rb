e53bd0e85a689a43336cf4db9be6d0c1df07c446
Alex Kravets <santyor@gmail.com>
2012-08-08 08:06:00 +0300

    admin fixes;

--- app/admin/company.rb
+++ app/admin/company.rb
@@ -17,34 +17,44 @@ ActiveAdmin.register Company do
   end
 
   form do |f|
-    f.inputs do
+    f.inputs "Details" do
       f.input :title, :required => true
       f.input :description, :as => :text, :required => true
+      f.input :url, :required => true
+    end
       
+    f.inputs "Address 1" do
       f.input :contact_title
       f.input :address
       f.input :phone, :required => true
       f.input :fax
       f.input :email, :required => true
+    end
       
+    f.inputs "Address 2" do
       f.input :contact_title2
       f.input :address2
       f.input :phone2
       f.input :fax2
       f.input :email2
+    end
 
+    f.inputs "Address 3" do
       f.input :contact_title3
       f.input :address3
       f.input :phone3
       f.input :fax3
       f.input :email3
-      
+    end
+
+    f.inputs "Map" do
       f.input :map_address
-      
+    end
+
+    f.inputs "Assets" do
       f.input :logo, :as => :file, :required => true
       f.input :banner, :as => :file, :required => true
       f.input :brochure, :as => :file
-      f.input :url, :required => true
     end
     f.buttons
   end

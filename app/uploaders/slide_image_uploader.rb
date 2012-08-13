f6ac0c5b56778e01766167a6e77d5d5f13992d4f
Alex Kravets <santyor@gmail.com>
2012-08-13 09:55:58 +0300

    update admin styles for slides;

--- app/uploaders/slide_image_uploader.rb
+++ app/uploaders/slide_image_uploader.rb
@@ -6,12 +6,12 @@ class SlideImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    
+    process :resize_to_fill => [290, 156]
+    process :quality => 80
   end
   
   version :full do
     process :resize_to_fill => [1500, 703]
     process :quality => 80
   end
-  
 end

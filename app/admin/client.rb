ab2d1a4a548efcd7af98d9ced0b1bc8cbf6bbbe1
Alex Kravets <santyor@gmail.com>
2012-08-13 12:34:20 +0300

    clients admin fix;

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -1,14 +1,21 @@
 ActiveAdmin.register Client do
   menu :parent 	=> "Corporation", :label => "Clients' Portfolio"
+
+  actions :all, :except => [:show]
   
   index do 
+    column "" do |obj|
+      
+    end
     column :title do |obj|
-      link_to obj.title, edit_admin_client_path(obj)
+      image_tag(obj.image.thumbnail)
     end
-    column :description
-    column :image do |obj|
-      img :src=>obj.image.thumbnail
+    column :description do |obj|
+      ( "<p> <strong> #{link_to(obj.title, edit_admin_client_path(obj))} </strong> </p>" +
+        obj.description ).html_safe
     end
+
+    default_actions
   end
 
   form do |f|
